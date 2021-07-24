2f87ee502cf36792dfd98ea1317b61bdbc65b196
Willians Magalhães Primo <87878484+williansmprimo@users.noreply.github.com>
2021-07-23 21:37:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var handlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
-var Post = require("./models/Post");
+//var Post = require("./models/Post");
 
 app.engine("handlebars", handlebars({defaultLayout: "index"}));
 app.set("view engine", "handlebars");
@@ -11,12 +11,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res){
-	Post.findAll().then(function(posts){
-		res.render("posts",{posts: posts});
-	});
+	//Post.findAll().then(function(posts){
+		//res.render("posts",{posts: posts});
+	//});
 	
 	//var a = Post.findAll();
 	//res.render("posts",{posts: a.rows});
+	var posts = [{titulo: "titulo", conteudo: "conteudo"}];
+	res.render("posts",{posts: posts});
 });
 
 app.get("/rota", function(req, res){
@@ -28,13 +30,14 @@ app.get("/rota", function(req, res){
 
 app.post("/cad", function(req, res){
 	//res.send("Titulo:" + req.body.nome + "Conteudo:" + req.body.telefone);
-	Post.create({titulo: req.body.titulo, conteudo: req.body.conteudo}).then(function(){
-		res.send("Cadastro com sucesso!");
-	}).catch(function(erro){
-		res.send("Houve erro: " + erro);
-	});
+	//Post.create({titulo: req.body.titulo, conteudo: req.body.conteudo}).then(function(){
+		//res.send("Cadastro com sucesso!");
+	//}).catch(function(erro){
+		//res.send("Houve erro: " + erro);
+	//});
+	res.send("Tudo certo!");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("escuntando na porta 3000!");
 });
