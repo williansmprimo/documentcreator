ea56580956c5188300d66a580e6ce2f65a111d8d
Willians Magalhães Primo <87878484+williansmprimo@users.noreply.github.com>
2021-07-23 21:30:06 -0300

    23072021

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var express = require("express");
 var app = express();
-//var handlebars = require("express-handlebars");
-//var bodyParser = require("body-parser");
-//var Post = require("./models/Post");
+var handlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+var Post = require("./models/Post");
 
-//app.engine("handlebars", handlebars({defaultLayout: "index"}));
-//app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars({defaultLayout: "index"}));
+app.set("view engine", "handlebars");
 
-//app.use(bodyParser.urlencoded({extended: false}));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res){
-	//Post.findAll().then(function(posts){
-		//res.render("posts",{posts: posts});
-		res.send("Hello Git/Heroku");
-	//});
+	Post.findAll().then(function(posts){
+		res.render("posts",{posts: posts});
+	});
 	
 	//var a = Post.findAll();
 	//res.render("posts",{posts: a.rows});
@@ -23,19 +22,19 @@ app.get("/", function(req, res){
 app.get("/rota", function(req, res){
 	//res.send("Hello JS World!");
 	//res.send("Hello JS World!");
-	//res.render("form");
+	res.render("form");
 	//res.json({name: "Willians", idade: 27});
 });
 
 app.post("/cad", function(req, res){
 	//res.send("Titulo:" + req.body.nome + "Conteudo:" + req.body.telefone);
-	//Post.create({titulo: req.body.titulo, conteudo: req.body.conteudo}).then(function(){
-		//res.send("Cadastro com sucesso!");
-	//}).catch(function(erro){
-		//res.send("Houve erro: " + erro);
-	/});
+	Post.create({titulo: req.body.titulo, conteudo: req.body.conteudo}).then(function(){
+		res.send("Cadastro com sucesso!");
+	}).catch(function(erro){
+		res.send("Houve erro: " + erro);
+	});
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(3000, function(){
 	console.log("escuntando na porta 3000!");
 });
